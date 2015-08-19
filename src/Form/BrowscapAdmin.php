c58864a07e27acc7fafc14832f932a969d365f73
Matt Lucasiewicz <mjl3475@users.noreply.github.com>
2015-08-19 11:01:48 -0400

    Fix some perfomance issues

--- src/Form/BrowscapAdmin.php
+++ src/Form/BrowscapAdmin.php
@@ -5,6 +5,7 @@ namespace Drupal\browscap\Form;
 use \Drupal\browscap\BrowscapImporter;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 
 class BrowscapAdmin extends ConfigFormBase {
@@ -58,8 +59,8 @@ class BrowscapAdmin extends ConfigFormBase {
       '#type' => 'select',
       '#title' => t('Check for new user agent detection information every'),
       '#default_value' => $config->get('browscap_automatic_updates_timer'),
-      '#options' => drupal_map_assoc(array(3600, 10800, 21600, 32400, 43200, 86400, 172800, 259200, 604800, 1209600, 2419200, 4838400, 9676800), 'format_interval'),
-      '#description' => t('Newer user agent detection information will be automatically downloaded and installed. (Requires a correctly configured <a href="@cron">cron maintenance task</a>.)', array('@cron' => url('admin/reports/status'))),
+      '#options' => array_map('format_interval',array(3600, 10800, 21600, 32400, 43200, 86400, 172800, 259200, 604800, 1209600, 2419200, 4838400, 9676800)),
+      '#description' => t('Newer user agent detection information will be automatically downloaded and installed. (Requires a correctly configured <a href="@cron">cron maintenance task</a>.)', array('@cron' => Url::fromRoute('system.status'))),
       '#states' => array(
         'visible' => array(
           ':input[name="browscap_enable_automatic_updates"]' => array('checked' => TRUE),
