45c976499ddd163e0e6575d973e7167c1f70ad43
matt.lucasiewicz <matt.lucasiewicz@acquia.com>
2015-08-20 09:36:48 -0400

    Fix links

--- src/Form/BrowscapAdmin.php
+++ src/Form/BrowscapAdmin.php
@@ -62,7 +62,7 @@ class BrowscapAdmin extends ConfigFormBase {
       '#title' => t('Check for new user agent detection information every'),
       '#default_value' => $config->get('browscap_automatic_updates_timer'),
       '#options' => array_map(array($dateformatter,'formatInterval'), array_combine($options, $options)),
-      //'#description' => t('Newer user agent detection information will be automatically downloaded and installed. (Requires a correctly configured <a href="@cron">cron maintenance task</a>.)', array('@cron' => Url::fromRoute('system.status')->getUri())),
+      '#description' => t('Newer user agent detection information will be automatically downloaded and installed. (Requires a correctly configured '. \Drupal::l("cron maintenance task", Url::fromRoute('system.status')) . '.'),
       '#states' => array(
         'visible' => array(
           ':input[name="browscap_enable_automatic_updates"]' => array('checked' => TRUE),
